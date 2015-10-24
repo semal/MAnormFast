c1693c2f21d860526368a0ffbb69cf42cfa9e1d2
semal <gzhsss2@gmail.com>
2015-10-24 16:09:13 +0800

    remove redundance

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from distutils.core import setup
-import sys
 
 
 def main():
-    if not float(sys.version[:3]) >= 2.5:
-        sys.stderr.write(
-            "CRITICAL: Python version must be greater than or equal to 2.5! python 2.6.1 is recommended!\n")
-        sys.exit(1)
 
     setup(
         name='MAnormFast',
